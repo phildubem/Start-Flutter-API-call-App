6ac0a3c994873c31c551b2624f0e08da683e55bb
Phil Dubem <phildubem@gmail.com>
2020-04-23 23:10:54 +0100

    UI with fetch

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,12 @@ import 'dart:async';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterapiapp/StringName.dart';
 import 'package:http/http.dart' as http;
 
 void main() => runApp(new MaterialApp(
   debugShowCheckedModeBanner: false,
-   title: "Data from API with json",
-   home: new HomePage(),
+  title: "Data from API with json",
+  home: new HomePage(),
 ));
 
 class HomePage extends StatefulWidget {
@@ -19,47 +18,83 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage>  {
 
+  final String url = "https://jsonplaceholder.typicode.com/users";
+  List data;
+
+
+  @override
+  void initState(){
+    super.initState();
+    this.getJsonData();
+  }
+
+  Future<String> getJsonData() async {
+
+    var response = await http.get(
+      Uri.encodeFull(url),
+    );
+
+    print(response.body);
+
+    setState(() {
+      var convertDataToJson = json.decode(response.body);
+      data = convertDataToJson;
+    });
+
+    return "Success";
+  }
+
 
   @override
   Widget build(BuildContext context) {
-
-   Future <StringName> fetchStringName() async {
-     final response = await http.get('https://jsonplaceholder.typicode.com/users');
-     
-
-   }
 
     return Scaffold (
       appBar: AppBar(
         title: Text("Data from API with json"),
       ),
       body: ListView.builder(
-          itemBuilder: (context,index) {
-            return Card (
-              child: Padding(
-                padding: const EdgeInsets.only(top: 23.0, bottom: 23.0, left: 16.0, right: 16.0),
-
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text("name",
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold
+        itemBuilder: (context,index) {
+          return Card (
+            child: Padding(
+              padding: const EdgeInsets.only(top: 23.0, bottom: 23.0, left: 16.0, right: 16.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Icon(
+                    Icons.person,
+                    size: 50.0,
+                    color: Colors.blueAccent,
+                  ),
+                  Column (
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text(data[index]["name"],
+                        style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold
+                        ),
                       ),
-                    ),
-                    Text("name",
-                    style: (TextStyle(
-                      color: Colors.grey.shade600,
-                    )),
-                    ),
-                  ],
-                ),
-
+                      Text(data[index]["email"],
+                        style: TextStyle(
+                          color: Colors.grey.shade600,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ],
+                  ),
+                  /*InkWell (
+                    onTap: () {
+                      Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MoreInfo()),
+                      );
+                    },
+                  ),*/
+                ],
               ),
-            );
-          },
-        itemCount: 10,
+            ),
+          );
+        },
+        itemCount: data == null ? 0 : data.length,
       ),
     );
   }
