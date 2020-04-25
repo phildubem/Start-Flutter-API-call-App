f8fd0d350a51e019ef9de38260d8b91daf6196e0
Phil Dubem <phildubem@gmail.com>
2020-04-25 15:06:50 +0100

    Final

--- lib/info.dart
+++ lib/info.dart
@@ -1,16 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapiapp/main.dart';
+
 
 
 class Info extends StatelessWidget {
+  final Model data;
+
+  Info(this.data);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold (
       appBar: AppBar(
-        title: Text("More Details"),
+        title: Text('Data for User ' + data.id.toString()),
       ),
-      body: Center (
-        child: Text(""),
+       body: Center (
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon (Icons.person_pin,
+            size: 90.0,
+              color: Colors.blue,
+            ),
+            Text(data.name,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+              ),
+            ),
+            Text (data.username,
+              style: TextStyle(
+                color: Colors.grey.shade900,
+                fontSize: 30,
+            ),
+            ),
+            Text (data.email,
+              style: TextStyle(
+                color: Colors.grey.shade800,
+                fontSize: 25,
+              ),
+            ),
+            Text (data.phone,
+              style: TextStyle(
+                color: Colors.grey.shade700,
+                fontSize: 25,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -6,102 +6,108 @@ import 'package:flutter/material.dart';
 import 'package:flutterapiapp/info.dart';
 import 'package:http/http.dart' as http;
 
+
+class Model {
+  String name;
+  String email;
+  String username;
+  String phone;
+  int id;
+
+
+  Model(this.name, this.email, this.username, this.phone, this.id);
+
+
+  Model.json(Map<String, dynamic> json) {
+    name= json['name'];
+    email = json['email'];
+    username = json['username'];
+    phone = json['phone'];
+    id =json['id'];
+  }
+}
 void main() => runApp(new MaterialApp(
   debugShowCheckedModeBanner: false,
   title: "Data from API with json",
   home: new HomePage(),
 ));
 
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
+
 class _HomePageState extends State<HomePage>  {
+  Future<List<Model>> fetchInfo() async {
+    var apiUrl = 'https://jsonplaceholder.typicode.com/users';
+    var result = await http.get(apiUrl);
 
-  final String url = "https://jsonplaceholder.typicode.com/users";
-  List data;
+    var users = List<Model>();
 
-
-  @override
-  void initState(){
-    super.initState();
-    this.getJsonData();
+    if (result.statusCode == 200) {
+      var usersJson = json.decode(result.body);
+      for (var user in usersJson) {
+        users.add(Model.json(user));
+      }
+    }
+    return users;
   }
-
-  Future<String> getJsonData() async {
-
-    var response = await http.get(
-      Uri.encodeFull(url),
-    );
-
-    print(response.body);
-
-    setState(() {
-      var convertDataToJson = json.decode(response.body);
-      data = convertDataToJson;
-    });
-
-    return "Success";
-  }
-
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold (
-      appBar: AppBar(
-        title: Text("Data from API with json"),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context,index) {
-          return Card (
-            child: InkWell (
-              onTap: () {
-                Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => Info(),
-                    settings: RouteSettings(
-                      arguments: data[index],
+        appBar: AppBar(
+          title: Text("Data from API with json"),
+        ),
+        body: FutureBuilder<List<dynamic>>(
+            future: fetchInfo(),
+            builder: (BuildContext context, AsyncSnapshot phil) {
+              return ListView.builder(
+                itemBuilder: (context,index) {
+                  return Card (
+                    child: InkWell (
+                      onTap: () {
+                        Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Info(phil.data[index])
+                          ),
+                        );
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 23.0, bottom: 23.0, left: 16.0, right: 16.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Icon(
+                              Icons.person,
+                              size: 50.0,
+                              color: Colors.blue,
+                            ),
+                            Column (
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(phil.data[index].name,
+                                  style: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                                Text(phil.data[index].email,
+                                  style: TextStyle(
+                                    color: Colors.grey.shade600,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                );
-              },
-
-             child: Padding(
-              padding: const EdgeInsets.only(top: 23.0, bottom: 23.0, left: 16.0, right: 16.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Icon(
-                    Icons.person,
-                    size: 50.0,
-                    color: Colors.blueAccent,
-                  ),
-                  Column (
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(data[index]["name"],
-                        style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold
-                        ),
-                      ),
-                      Text(data[index]["email"],
-                        style: TextStyle(
-                          color: Colors.grey.shade600,
-                          fontWeight: FontWeight.normal,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-           ),
-          );
-        },
-        itemCount: data == null ? 0 : data.length,
-      ),
-    );
+                  );
+                },
+                itemCount: phil.data == null ? 0 : phil.data.length,
+              );
+            }));
   }
 }
